946cde596402393cd09f6d577af26babbab3fefa
Vokuar <mindblast213@gmail.com>
2023-05-23 11:17:58 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,3 +19,43 @@ if [[ $download_python == [Yy] ]]; then
         if command -v apt >/dev/null 2>&1; then
             echo "Installing Python using apt..."
             sudo apt update
+            sudo apt install python3 -y
+        elif command -v yum >/dev/null 2>&1; then
+            echo "Installing Python using yum..."
+            sudo yum update
+            sudo yum install python3 -y
+        else
+            echo "Unable to determine the package manager. Falling back to manual installation..."
+            
+            # Download Python from the web
+            echo "Downloading Python..."
+            wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tar.xz -O python.tar.xz
+            if [ ! -f python.tar.xz ]; then
+                echo "Failed to download Python. Exiting..."
+                exit 1
+            fi
+            echo "Python downloaded successfully."
+
+            echo "Installing Python..."
+            tar -xf python.tar.xz
+            cd Python-3.10.0
+            ./configure --prefix=$HOME/.python
+            make -j$(nproc)
+            make install
+            cd ..
+            if [ ! -d $HOME/.python ]; then
+                echo "Failed to install Python. Exiting..."
+                exit 1
+            fi
+            echo "Python installed successfully."
+        fi
+    fi
+else
+    echo "Skipping Python installation."
+fi
+
+echo
+echo "Starting Minecraft server installer..."
+echo
+
+python3 minecraft_server_installer.py
